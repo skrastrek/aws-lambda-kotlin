0722dd67401b58eaeee8aabb240a90aff6cb230b
SebRamsland <mail@sebastianramsland.com>
2024-11-22 12:37:58 +0100

    Initial setup of GitHub Actions workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ allprojects {
     group = "io.skrastrek.aws.lambda.kotlin"
     repositories {
         mavenCentral()
-        google()
     }
 }
 
@@ -20,6 +19,71 @@ plugins {
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
+
+    configure<SigningExtension> {
+        val signingKey = providers.environmentVariable("GPG_SIGNING_KEY")
+        val signingPassphrase = providers.environmentVariable("GPG_SIGNING_PASSPHRASE")
+
+        if (signingKey.isPresent && signingPassphrase.isPresent) {
+            useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+            val extension = extensions.getByName("publishing") as PublishingExtension
+            sign(extension.publications)
+        }
+    }
+
+    configure<PublishingExtension> {
+        publications.register<MavenPublication>("maven") {
+            artifactId = project.name
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            pom {
+                name = "AWS Lambda Kotlin"
+                description = "Utility for Kotlin development with AWS Lambda."
+                url = "https://github.com/skrastrek/aws-lambda-kotlin"
+                packaging = "jar"
+
+                scm {
+                    url = "https://github.com/skrastrek/aws-lambda-kotlin"
+                    connection = "scm:git://github.com:skrastrek/aws-lambda-kotlin.git"
+                    developerConnection = "scm:git://github.com:skrastrek/aws-lambda-kotlin.git"
+                }
+
+                licenses {
+                    license {
+                        name = "Apache-2.0"
+                        url = "https://opensource.org/licenses/Apache-2.0"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "sebramsland"
+                        name = "Sebastian Ramsland"
+                        email = "sebastian@skrastrek.io"
+                        organizationUrl = "https://skrastrek.io"
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                val releaseRepo = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                val snapshotRepo = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+
+                name = "OSSRH"
+                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotRepo else releaseRepo)
+
+                credentials {
+                    username = System.getenv("OSSRH_USERNAME")
+                    password = System.getenv("OSSRH_PASSWORD")
+                }
+            }
+        }
+    }
 
     tasks.withType<Test> {
         useJUnitPlatform()
