b58ec835cf3944d74d602f0415c8aeb515fce797
SebRamsland <mail@sebastianramsland.com>
2024-11-25 12:37:24 +0100

    Remove OSSHR from maven publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,15 +76,6 @@ subprojects {
                     password = System.getenv("GITHUB_TOKEN")
                 }
             }
-
-            maven {
-                name = "OSSRH"
-                url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-                credentials {
-                    username = System.getenv("OSSRH_USERNAME")
-                    password = System.getenv("OSSRH_PASSWORD")
-                }
-            }
         }
     }
 
